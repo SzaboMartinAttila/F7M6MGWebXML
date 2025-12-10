c90658268d5246270b832bae67b7600e99233a0d
SzaboMartinAttila <149079305+SzaboMartinAttila@users.noreply.github.com>
2025-12-10 10:41:29 +0100

    Add files via upload

--- F7M6MG_XMLTask/F7M6MGDOMParse/src/f7m6mg/domparse/hu/F7M6MGDOMRead.java
+++ F7M6MG_XMLTask/F7M6MGDOMParse/src/f7m6mg/domparse/hu/F7M6MGDOMRead.java
@@ -8,118 +8,119 @@ import javax.xml.transform.stream.StreamResult;
 import java.io.*;
 
 /**
- * F7M6MG - DOM Parser XML olvasás
- * Ez az osztály beolvassa az XML dokumentumot, kiírja a konzolra formázottan,
+ * F7M6MG - DOM Parser XML olvasás (Blokkosított/Strukturált kimenet)
+ * Ez az osztály beolvassa az XML dokumentumot, kiírja a konzolra OLVASHATÓ formában,
  * és elmenti egy új fájlba.
  */
 public class F7M6MGDOMRead {
 
     public static void main(String[] args) {
         try {
-            // DocumentBuilderFactory létrehozása
+            // 1. XML fájl betöltése
             DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
             DocumentBuilder builder = factory.newDocumentBuilder();
-
-            // XML fájl beolvasása
-            File xmlFile = new File("F7M6MG_XML.xml");
+            File xmlFile = new File("F7M6MG_XML.xml"); // A bemeneti fájl neve
             Document document = builder.parse(xmlFile);
-
-            // Dokumentum normalizálása
             document.getDocumentElement().normalize();
 
-            System.out.println("=== F7M6MG - XML Dokumentum Beolvasása ===\n");
-            System.out.println("Gyökér elem: " + document.getDocumentElement().getNodeName());
-            System.out.println("\n--- Teljes dokumentum tartalma ---\n");
+            System.out.println("=== F7M6MG - XML Tartalom (Strukturált Nézet) ===\n");
 
-            // Teljes dokumentum kiírása a konzolra
+          
             printDocument(document.getDocumentElement(), "");
 
-            // Dokumentum mentése új fájlba
+            
             saveDocument(document, "F7M6MGXML_Saved.xml");
 
             System.out.println("\n=== Sikeres művelet! ===");
             System.out.println("Az XML dokumentum elmentve: F7M6MGXML_Saved.xml");
 
         } catch (Exception e) {
-            System.err.println("Hiba történt az XML feldolgozása során:");
+            System.err.println("Hiba történt: " + e.getMessage());
             e.printStackTrace();
         }
     }
 
     /**
-     * Rekurzív függvény az XML fa bejárására és konzolra írására
-     * 
-     * @param node   Az aktuális csomópont
-     * @param indent Behúzás a formázott megjelenítéshez
+     * Rekurzív metódus a fa bejárására és blokkosított kiírására
      */
     private static void printDocument(Node node, String indent) {
-        // Csomópont típusának ellenőrzése
         if (node.getNodeType() == Node.ELEMENT_NODE) {
             Element element = (Element) node;
+            String nodeName = element.getNodeName();
 
-            // Elem neve és attribútumai
-            System.out.print(indent + "<" + element.getNodeName());
+            
+            boolean hasChildElements = hasChildElements(element);
 
-            // Attribútumok kiírása, ha vannak
-            NamedNodeMap attributes = element.getAttributes();
-            if (attributes.getLength() > 0) {
-                for (int i = 0; i < attributes.getLength(); i++) {
-                    Node attr = attributes.item(i);
-                    System.out.print(" " + attr.getNodeName() + "=\"" + attr.getNodeValue() + "\"");
+            if (hasChildElements) {
+      
+                System.out.println(indent + "-------------------------");
+                
+                // Név kiírása nagybetűvel
+                System.out.print(indent + nodeName.toUpperCase());
+
+                // Attribútumok kiírása mellé (ha vannak)
+                if (element.hasAttributes()) {
+                    NamedNodeMap attrs = element.getAttributes();
+                    for (int i = 0; i < attrs.getLength(); i++) {
+                        Node attr = attrs.item(i);
+                        System.out.print(" | " + attr.getNodeName() + ": " + attr.getNodeValue());
+                    }
                 }
-            }
-            System.out.print(">");
+                System.out.println(); // Sorzárás
 
-            // Gyerek csomópontok feldolgozása
-            NodeList children = element.getChildNodes();
-            boolean hasElementChildren = false;
-
-            // Ellenőrizzük, hogy vannak-e elem gyerekek
-            for (int i = 0; i < children.getLength(); i++) {
-                if (children.item(i).getNodeType() == Node.ELEMENT_NODE) {
-                    hasElementChildren = true;
-                    break;
-                }
-            }
-
-            if (hasElementChildren) {
-                System.out.println(); // Új sor, ha vannak gyerek elemek
+                // Rekurzív hívás a gyerekekre
+                NodeList children = element.getChildNodes();
                 for (int i = 0; i < children.getLength(); i++) {
                     printDocument(children.item(i), indent + "  ");
                 }
-                System.out.println(indent + "</" + element.getNodeName() + ">");
+
             } else {
-                // Szöveges tartalom kiírása
+                // --- ADAT ELEM (pl. Nev, Iranyitoszam) ---
                 String textContent = element.getTextContent().trim();
+                
                 if (!textContent.isEmpty()) {
-                    System.out.print(textContent);
+                    System.out.print(indent + nodeName + ": " + textContent);
+                    
+                    // Ritka eset: ha egy adatmezőnek attribútuma is van
+                    if (element.hasAttributes()) {
+                        NamedNodeMap attrs = element.getAttributes();
+                        System.out.print(" (");
+                        for (int i = 0; i < attrs.getLength(); i++) {
+                            Node attr = attrs.item(i);
+                            System.out.print(attr.getNodeName() + "=" + attr.getNodeValue() + " ");
+                        }
+                        System.out.print(")");
+                    }
+                    System.out.println();
                 }
-                System.out.println("</" + element.getNodeName() + ">");
             }
         }
     }
 
     /**
-     * XML dokumentum mentése fájlba
-     * 
-     * @param document A mentendő dokumentum
-     * @param filename A célfájl neve
+     * Segédfüggvény: Megnézi, hogy van-e az elemnek további gyerek TAG-je.
+     */
+    private static boolean hasChildElements(Element el) {
+        NodeList children = el.getChildNodes();
+        for (int i = 0; i < children.getLength(); i++) {
+            if (children.item(i).getNodeType() == Node.ELEMENT_NODE) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Mentés fájlba (Ez változatlan, XML formátumban ment)
      */
     private static void saveDocument(Document document, String filename) throws TransformerException {
-        // Transformer létrehozása a mentéshez
         TransformerFactory transformerFactory = TransformerFactory.newInstance();
         Transformer transformer = transformerFactory.newTransformer();
-
-        // Formázási beállítások
         transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-        transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
         transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
-
-        // DOM forrás és fájl cél
+        
         DOMSource source = new DOMSource(document);
         StreamResult result = new StreamResult(new File(filename));
-
-        // Mentés végrehajtása
         transformer.transform(source, result);
     }
 }
